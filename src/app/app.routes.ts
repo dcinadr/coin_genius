f430c3ce1a380e558e9aeddb27feeade773cd011
David Cinadr <dcinadr@gmail.com>
2018-01-24 21:57:53 -0800

    integrating coin genius dashboard mockup from wire frames

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,8 @@ import {Dashboard2Component} from "./views/dashboards/dashboard2.component";
 import {Dashboard3Component} from "./views/dashboards/dashboard3.component";
 import {Dashboard4Component} from "./views/dashboards/dashboard4.component";
 import {Dashboard41Component} from "./views/dashboards/dashboard41.component";
-import {Dashboard5Component} from "./views/dashboards/dashboard5.component";
+import { Dashboard5Component } from "./views/dashboards/dashboard5.component";
+import { DashboardCgComponent } from "./views/dashboards/dashboardCg.component";
 
 import {StarterViewComponent} from "./views/appviews/starterview.component";
 import {LoginComponent} from "./views/appviews/login.component";
@@ -26,7 +27,8 @@ export const ROUTES:Routes = [
       {path: 'dashboard2', component: Dashboard2Component},
       {path: 'dashboard3', component: Dashboard3Component},
       {path: 'dashboard4', component: Dashboard4Component},
-      {path: 'dashboard5', component: Dashboard5Component}
+      { path: 'dashboard5', component: Dashboard5Component },
+      { path: 'dashboardCg', component: DashboardCgComponent }
     ]
   },
   {
--- src/app/views/dashboards/dashboards.module.ts
+++ src/app/views/dashboards/dashboards.module.ts
@@ -6,7 +6,8 @@ import {Dashboard2Component} from "./dashboard2.component";
 import {Dashboard3Component} from "./dashboard3.component";
 import {Dashboard4Component} from "./dashboard4.component";
 import {Dashboard41Component} from "./dashboard41.component";
-import {Dashboard5Component} from "./dashboard5.component";
+import { Dashboard5Component } from "./dashboard5.component";
+import { DashboardCgComponent } from "./dashboardCg.component";
 
 // Chart.js Angular 2 Directive by Valor Software (npm)
 import { ChartsModule } from 'ng2-charts/ng2-charts';
@@ -19,9 +20,9 @@ import { JVectorMapModule } from '../../components/map/jvectorMap';
 
 
 @NgModule({
-  declarations: [Dashboard1Component,Dashboard2Component,Dashboard3Component,Dashboard4Component,Dashboard41Component,Dashboard5Component],
+  declarations: [Dashboard1Component,Dashboard2Component,Dashboard3Component,Dashboard4Component,Dashboard41Component,Dashboard5Component,DashboardCgComponent],
   imports     : [BrowserModule,ChartsModule, FlotModule,IboxtoolsModule,PeityModule,SparklineModule,JVectorMapModule],
-  exports     : [Dashboard1Component,Dashboard2Component,Dashboard3Component,Dashboard4Component,Dashboard41Component,Dashboard5Component],
+  exports     : [Dashboard1Component,Dashboard2Component,Dashboard3Component,Dashboard4Component,Dashboard41Component,Dashboard5Component,DashboardCgComponent],
 })
 
 export class DashboardsModule {}
